0fedc783b5e549116bd90a20d647b099e8b85645
Christopher Collins <ccollins476ad@gmail.com>
2016-03-01 18:28:10 -0800

    Add missing apache license to samd21.h.

--- hw/mcu/atmel/samd21xx/include/mcu/samd21.h
+++ hw/mcu/atmel/samd21xx/include/mcu/samd21.h
@@ -1,8 +1,20 @@
-/* 
- * File:   samd21.h
- * Author: paulfdietrich
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Created on February 3, 2016, 6:01 PM
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 #ifndef SAMD21_H
@@ -13,9 +25,9 @@ extern "C" {
 #endif
 
 #include "../../src/sam0/utils/cmsis/samd21/include/samd21.h"
-    
-extern struct hal_flash samd21_flash_dev; 
-    
+
+extern struct hal_flash samd21_flash_dev;
+
 #ifdef	__cplusplus
 }
 #endif
