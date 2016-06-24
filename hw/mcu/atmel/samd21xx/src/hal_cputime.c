55af7badceb6d29d53af7863fd8527cf813d1c12
William San Filippo <wills@runtime.io>
2016-06-23 23:08:02 -0700

    MYNEWT-79: fix arduino zero build

--- hw/mcu/atmel/samd21xx/src/hal_cputime.c
+++ hw/mcu/atmel/samd21xx/src/hal_cputime.c
@@ -6,7 +6,7 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
@@ -27,6 +27,7 @@
 #include "sam0/utils/status_codes.h"
 #include "common/utils/interrupt/interrupt_sam_nvic.h"
 
+struct tc_module g_cputime_timer;
 
 /* XXX:
  *  - Must determine how to set priority of cpu timer interrupt
@@ -35,70 +36,65 @@
  *  - Sync to OSTIME.
  */
 
-/* CPUTIME data */
-struct cputime_data
+void
+cputime_disable_ocmp(void)
 {
-    uint32_t ticks_per_usec;    /* number of ticks per usec */
-    uint32_t cputime_high;      /* high word of 64-bit cpu time */
-    struct tc_module timer;
-};
-
-
-struct cputime_data g_cputime;
+    /* XXX: implement */
+}
 
 /**
- * cputime init 
- *  
- * Initialize the cputime module. This must be called after os_init is called 
- * and before any other timer API are used. This should be called only once 
- * and should be called before the hardware timer is used. 
- * 
+ * cputime init
+ *
+ * Initialize the cputime module. This must be called after os_init is called
+ * and before any other timer API are used. This should be called only once
+ * and should be called before the hardware timer is used.
+ *
  * @param clock_freq The desired cputime frequency, in hertz (Hz).
- * 
+ *
  * @return int 0 on success; -1 on error.
  */
 int
-cputime_init(uint32_t clock_freq)
+cputime_hw_init(uint32_t clock_freq)
 {
     enum status_code rc;
     struct system_gclk_gen_config gcfg;
-    
+
     struct tc_config cfg;
-    tc_get_config_defaults(&cfg);  
-    
+    tc_get_config_defaults(&cfg);
+
     if(clock_freq > 8000000) {
         return -1;
     }
-    
+
     /* set up gclk generator 1 to source this timer */
     gcfg.division_factor = 1;
     gcfg.high_when_disabled = false;
     gcfg.output_enable = true;
     gcfg.run_in_standby = true;
-    gcfg.source_clock = GCLK_SOURCE_OSC8M;    
+    gcfg.source_clock = GCLK_SOURCE_OSC8M;
     system_gclk_gen_set_config(GCLK_GENERATOR_1, &gcfg);
-    
+
     cfg.counter_size = TC_COUNTER_SIZE_32BIT;
-    cfg.clock_source = GCLK_GENERATOR_1;       
+    cfg.clock_source = GCLK_GENERATOR_1;
     cfg.clock_prescaler = TC_CLOCK_PRESCALER_DIV1;
-    
+
     g_cputime.ticks_per_usec = 8;
-    
-    rc = tc_init(&g_cputime.timer, TC4, &cfg);
-    
-    tc_enable(&g_cputime.timer);   
-    
+
+    rc = tc_init(&g_cputime_timer, TC4, &cfg);
+
+    tc_enable(&g_cputime_timer);
+
     return rc;
 }
 
 /**
  * cputime get64
- *  
- * Returns cputime as a 64-bit number. 
- * 
+ *
+ * Returns cputime as a 64-bit number.
+ *
  * @return uint64_t The 64-bit representation of cputime.
  */
-uint64_t 
+uint64_t
 cputime_get64(void)
 {
     uint32_t high;
@@ -120,10 +116,10 @@ cputime_get64(void)
 }
 
 /**
- * cputime get32 
- *  
- * Returns the low 32 bits of cputime. 
- * 
+ * cputime get32
+ *
+ * Returns the low 32 bits of cputime.
+ *
  * @return uint32_t The lower 32 bits of cputime
  */
 uint32_t
@@ -131,131 +127,7 @@ cputime_get32(void)
 {
     uint32_t cpu_time;
 
-    cpu_time = tc_get_count_value(&g_cputime.timer);    
+    cpu_time = tc_get_count_value(&g_cputime_timer);
 
     return cpu_time;
-}
-
-/**
- * cputime nsecs to ticks 
- *  
- * Converts the given number of nanoseconds into cputime ticks. 
- * 
- * @param usecs The number of nanoseconds to convert to ticks
- * 
- * @return uint32_t The number of ticks corresponding to 'nsecs'
- */
-uint32_t 
-cputime_nsecs_to_ticks(uint32_t nsecs)
-{
-    uint32_t ticks;
-
-    ticks = ((nsecs * g_cputime.ticks_per_usec) + 999) / 1000;
-    return ticks;
-}
-
-/**
- * cputime ticks to nsecs
- *  
- * Convert the given number of ticks into nanoseconds. 
- * 
- * @param ticks The number of ticks to convert to nanoseconds.
- * 
- * @return uint32_t The number of nanoseconds corresponding to 'ticks'
- */
-uint32_t 
-cputime_ticks_to_nsecs(uint32_t ticks)
-{
-    uint32_t nsecs;
-
-    nsecs = ((ticks * 1000) + (g_cputime.ticks_per_usec - 1)) / 
-            g_cputime.ticks_per_usec;
-
-    return nsecs;
-}
-
-/**
- * cputime usecs to ticks 
- *  
- * Converts the given number of microseconds into cputime ticks. 
- * 
- * @param usecs The number of microseconds to convert to ticks
- * 
- * @return uint32_t The number of ticks corresponding to 'usecs'
- */
-uint32_t 
-cputime_usecs_to_ticks(uint32_t usecs)
-{
-    uint32_t ticks;
-
-    ticks = (usecs * g_cputime.ticks_per_usec);
-    return ticks;
-}
-
-/**
- * cputime ticks to usecs
- *  
- * Convert the given number of ticks into microseconds. 
- * 
- * @param ticks The number of ticks to convert to microseconds.
- * 
- * @return uint32_t The number of microseconds corresponding to 'ticks'
- */
-uint32_t 
-cputime_ticks_to_usecs(uint32_t ticks)
-{
-    uint32_t us;
-
-    us =  (ticks + (g_cputime.ticks_per_usec - 1)) / g_cputime.ticks_per_usec;
-    return us;
-}
-
-/**
- * cputime delay ticks
- *  
- * Wait until the number of ticks has elapsed. This is a blocking delay. 
- * 
- * @param ticks The number of ticks to wait.
- */
-void 
-cputime_delay_ticks(uint32_t ticks)
-{
-    uint32_t until;
-
-    until = cputime_get32() + ticks;
-    while ((int32_t)(cputime_get32() - until) < 0) {
-        /* Loop here till finished */
-    }
-}
-
-/**
- * cputime delay nsecs 
- *  
- * Wait until 'nsecs' nanoseconds has elapsed. This is a blocking delay. 
- *  
- * @param nsecs The number of nanoseconds to wait.
- */
-void 
-cputime_delay_nsecs(uint32_t nsecs)
-{
-    uint32_t ticks;
-
-    ticks = cputime_nsecs_to_ticks(nsecs);
-    cputime_delay_ticks(ticks);
-}
-
-/**
- * cputime delay usecs 
- *  
- * Wait until 'usecs' microseconds has elapsed. This is a blocking delay. 
- *  
- * @param usecs The number of usecs to wait.
- */
-void 
-cputime_delay_usecs(uint32_t usecs)
-{
-    uint32_t ticks;
-
-    ticks = cputime_usecs_to_ticks(usecs);
-    cputime_delay_ticks(ticks);
 }
