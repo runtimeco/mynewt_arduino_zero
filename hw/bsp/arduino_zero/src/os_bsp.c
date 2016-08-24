976ed190e4e30171a0c2444b6f59022413434849
Marko Kiiskila <marko@runtime.io>
2016-08-24 12:38:21 -0700

    arduino_zero; create uart0 os device in bsp_init().

--- hw/bsp/arduino_zero/src/os_bsp.c
+++ hw/bsp/arduino_zero/src/os_bsp.c
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,7 +7,7 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
@@ -18,8 +18,16 @@
  * under the License.
  */
 #include <sys/types.h>
+#include <assert.h>
+
 #include <hal/hal_bsp.h>
 #include <hal/flash_map.h>
+
+#include <os/os_dev.h>
+#include <uart/uart.h>
+#include <uart_hal/uart_hal.h>
+
+#include "bsp/bsp.h"
 
 void _close(int fd);
 
@@ -56,6 +64,10 @@ static struct flash_area arduino_zero_flash_areas[] = {
     },
 };
 
+/* XXX should not be declared here */
+extern const struct samd21_uart_config *bsp_uart_config(int port);
+static struct uart_dev hal_uart0;
+
 int
 bsp_imgr_current_slot(void)
 {
@@ -65,6 +77,8 @@ bsp_imgr_current_slot(void)
 void
 bsp_init(void)
 {
+    int rc;
+
     /*
      * XXX these references are here to keep the functions in for libc to find.
      */
@@ -72,4 +86,8 @@ bsp_init(void)
     _close(0);
     flash_area_init(arduino_zero_flash_areas,
       sizeof(arduino_zero_flash_areas) / sizeof(arduino_zero_flash_areas[0]));
+
+    rc = os_dev_create((struct os_dev *) &hal_uart0, CONSOLE_UART,
+      OS_DEV_INIT_PRIMARY, 0, uart_hal_init, (void *)bsp_uart_config(0));
+    assert(rc == 0);
 }
