1635c2723d859120108e7a7928583868f81eceef
Marko Kiiskila <marko@runtime.io>
2016-09-08 15:47:52 -0700

    MYNEWT-241; allow user to pass additional commands to JTAG adapter SW. These can be used to select a specific target. Changed the way parameters get passed to download/debug script. Now they come in as environment variables, instead of command line.

--- hw/bsp/arduino_zero/arduino_zero_download.sh
+++ hw/bsp/arduino_zero/arduino_zero_download.sh
@@ -16,35 +16,37 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-# Called: $0 <bsp_directory_path> <binary> [features...]
-#  - bsp_directory_path is absolute path to hw/bsp/bsp_name
-#  - binary is the path to prefix to target binary, .elf appended to name is
-#    the ELF file
-#  - identities is the project identities string.
+# Called with following variables set:
+#  - BSP_PATH is absolute path to hw/bsp/bsp_name
+#  - BIN_BASENAME is the path to prefix to target binary,
+#    .elf appended to name is the ELF file
+#  - IMAGE_SLOT is the image slot to download to
+#  - FEATURES holds the target features string
+#  - EXTRA_JTAG_CMD holds extra parameters to pass to jtag software
 #
 #
-if [ $# -lt 2 ]; then
+IS_BOOTLOADER=0
+
+if [ -z "$BIN_BASENAME" ]; then
     echo "Need binary to download"
     exit 1
 fi
 
-MY_PATH=$1
-BASENAME=$2
-IS_BOOTLOADER=0
+if [ -z "$BSP_PATH" ]; then
+    echo "Need BSP path for openocd script location"
+    exit 1
+fi
 
-# Look for 'bootloader' from 3rd arg onwards
-shift
-shift
-while [ $# -gt 0 ]; do
-    if [ $1 == "bootloader" ]; then
+# Look for 'bootloader' in FEATURES
+for feature in $FEATURES; do
+    if [ $feature == "bootloader" ]; then
 	IS_BOOTLOADER=1
     fi
-    shift
 done
 
 if [ $IS_BOOTLOADER -eq 1 ]; then
     FLASH_OFFSET=0x00000000
-    FILE_NAME=$BASENAME.elf.bin
+    FILE_NAME=$BIN_BASENAME.elf.bin
     # we will unprotect and reprotect our bootloader to ensure that its
     # safe. also Arduino Zero Pro boards looks like they come with the 
     # arduino bootloader protected via the NVM AUX 
@@ -54,10 +56,18 @@ if [ $IS_BOOTLOADER -eq 1 ]; then
 else
     # this number is to offset the bootloader size 
     FLASH_OFFSET=0x0000C000
-    FILE_NAME=$BASENAME.img
+    FILE_NAME=$BIN_BASENAME.img
 fi
+
 echo "Downloading" $FILE_NAME "to" $FLASH_OFFSET
 
-openocd -f $MY_PATH/arduino_zero.cfg -c init -c "reset halt" $CMD "$UNPROTECT_FLASH" -c "reset halt" -c "flash write_image erase $FILE_NAME $FLASH_OFFSET" $CMD "$PROTECT_FLASH" -c "reset run" -c shutdown
+# Extra parameters to pass to openocd
+if [ ! -z "$EXTRA_JTAG_CMD" ]; then
+    EXTRA_CMD="-c $EXTRA_JTAG_CMD"
+else
+    EXTRA_CMD=
+fi
+
+openocd -f $BSP_PATH/arduino_zero.cfg $EXTRA_CMD -c init -c "reset halt" $CMD "$UNPROTECT_FLASH" -c "reset halt" -c "flash write_image erase $FILE_NAME $FLASH_OFFSET" $CMD "$PROTECT_FLASH" -c "reset run" -c shutdown | xargs -n 1 echo 
 
 
