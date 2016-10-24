f53f48ad82d80a0348ad5dab8538168a8cb7f587
Marko Kiiskila <marko@runtime.io>
2016-10-24 15:50:02 -0700

    apps/arduino_test; use sysinit(), get it to compile with new pkg locations.

--- apps/arduino_test/src/main.c
+++ apps/arduino_test/src/main.c
@@ -19,52 +19,23 @@
 #include <os/os.h>
 #include <bsp/bsp.h>
 #include <console/console.h>
-#include <shell/shell.h>
 #include <log/log.h>
 #include <config/config.h>
-#include <newtmgr/newtmgr.h>
 #include <arduino_test/arduino_test.h>
 #include <assert.h>
 #include <string.h>
+
+#include <sysinit/sysinit.h>
 
 #ifdef ARCH_sim
 #include <mcu/mcu_sim.h>
 #endif
 
-/* Init all tasks */
-volatile int tasks_initialized;
-int init_tasks(void);
-
-#define SHELL_TASK_PRIO (3)
-#define SHELL_MAX_INPUT_LEN     (256)
-#define SHELL_TASK_STACK_SIZE (OS_STACK_ALIGN(384))
-os_stack_t shell_stack[SHELL_TASK_STACK_SIZE];
-
-#define NEWTMGR_TASK_PRIO (4)
-#define NEWTMGR_TASK_STACK_SIZE (OS_STACK_ALIGN(512))
-os_stack_t newtmgr_stack[NEWTMGR_TASK_STACK_SIZE];
-
-/**
- * init_tasks
- *
- * Called by main.c after os_init(). This function performs initializations
- * that are required before tasks are running.
- *
- * @return int 0 success; error otherwise.
- */
-int
-init_tasks(void)
-{
-    tasks_initialized = 1;
-    return 0;
-}
-
 /**
  * main
  *
- * The main function for the project. This function initializes the os, calls
- * init_tasks to initialize tasks (and possibly other objects), then starts the
- * OS. We should not return from os start.
+ * The main function for the project. This function initializes the system,
+ * then starts the OS. We should not return from os start.
  *
  * @return int NOTE: this function should never return!
  */
@@ -77,17 +48,11 @@ main(int argc, char **argv)
     mcu_sim_parse_args(argc, argv);
 #endif
 
-    os_init();
-
-    shell_task_init(SHELL_TASK_PRIO, shell_stack, SHELL_TASK_STACK_SIZE,
-                    SHELL_MAX_INPUT_LEN);
-
-    nmgr_task_init(NEWTMGR_TASK_PRIO, newtmgr_stack, NEWTMGR_TASK_STACK_SIZE);
+    sysinit();
 
     rc = arduino_test_init();
     assert(rc == 0);
 
-    rc = init_tasks();
     os_start();
 
     /* os start should never return. If it does, this should be an error */
